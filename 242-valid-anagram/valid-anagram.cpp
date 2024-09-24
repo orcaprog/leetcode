a3a819d354ed7de0fba66b71593392b00bc2c326
Ayoub bouassid <118219250+orcaprog@users.noreply.github.com>
2024-09-24 23:05:24 +0100

    Time: 13 ms (26.32%) | Memory: 8.7 MB (84.12%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-    map<char, int> mp1;
-    map<char, int> mp2;
-    if(s.size() !=  t.size())
+    if(s.size() != t.size()){return false;} 
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+
+    size_t found = s.find(t);
+    if (found != string::npos)
     {
-        return false;
-    }
-    int i =0 ;
-    while(s[i])
-    {
-        mp1[s[i]] += 1; 
-        mp2[t[i]] += 1; 
-        i++;
-    }
-    map<char, int>::iterator it1 = mp1.begin();
-    map<char, int>::iterator it2 = mp2.begin();
-    if(mp1.size() > mp2.size())
-    {
-        return false;
-    }
-    while (it2 != mp2.end()) 
-    {
-    
-        if(it2->first != it1->first ||it2->second < it1->second )
-        {
-            return false;
-        }
-        it2++;
-        it1++;
-    }
         return true;
+    }
+    return false;
     }
 };
