844bbf54cb295cf088a8001153a727f8a7f0e857
Ayoub bouassid <118219250+orcaprog@users.noreply.github.com>
2025-08-23 11:47:57 +0100

    Time: 0 ms (100.00%) | Memory: 18.9 MB (50.25%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,7 +5,6 @@ class Solution:
 
         for i, num in enumerate(nums):
             diff = target - num
-            print(i)
             if diff in seen:
                 return [seen[diff], i]
             seen[num] = i
